5ebd9a3adfdc842cd2a6fb9ebb4dcce09abadf57
An <144849573+An90456@users.noreply.github.com>
2023-09-28 19:03:00 +0100

    Add files via upload

--- search.py
+++ search.py
@@ -98,14 +98,20 @@ def graph_search(problem, fringe):
     The argument fringe should be an empty queue.
     If two paths reach a state, only use the best one. [Fig. 3.18]"""
     closed = {}
+    numeroNodosExpandidos = 1
+    numeroNodosVisitados = 1
     fringe.append(Node(problem.initial))
     while fringe:
         node = fringe.pop()
         if problem.goal_test(node.state):
+            print("Numero de nodos visitado:"+str(numeroNodosVisitados))
+            print("Numero de nodos expandidos:"+str(numeroNodosExpandidos))
             return node
         if node.state not in closed:
             closed[node.state] = True
             fringe.extend(node.expand(problem))
+            numeroNodosExpandidos+=len(node.expand(problem))
+        numeroNodosVisitados+=1
     return None
 
 
